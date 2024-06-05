1735f96b67371b9aef43490b77f90ba1baee3701
rahul_thakur <rahulthakur.developer@gmail.com>
2024-06-05 12:28:45 +0530

    prisma setup done

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import {expressMiddleware} from '@apollo/server/express4'
+import {primsaClient} from './lib/db';
 
 async function init(){
     const app = express();
@@ -14,11 +15,30 @@ async function init(){
                 hello: String
                 say(name: String): String
             }
+            type Mutation{
+                createUser(firstName: String!, lastName: String! , email : String!, password: String!): Boolean
+            }
         `,
         resolvers: {
             Query: {
                 hello: () => 'Hello there i am a graphql server!',
                 say: (_,{name}: {name:String}) => `Hello ${name} i am a graphql server!`
+            },
+            Mutation: {
+                createUser:async (_,
+                    {firstName, lastName, email, password}: {firstName: string, lastName: string, email: string, password: string}) => {
+                        await primsaClient.user.create({
+                            data: {
+                                firstName,
+                                lastName,
+                                email,
+                                password,
+                                salt: 'random_salt'
+                            }
+                        });
+                    console.log(firstName, lastName, email, password);
+                    return true;
+                }
             }
         }
     });
---
+++ src/lib/db.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.primsaClient = void 0;
+var client_1 = require("@prisma/client");
+exports.primsaClient = new client_1.PrismaClient();
