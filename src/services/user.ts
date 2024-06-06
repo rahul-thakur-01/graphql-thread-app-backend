41f23046d69f680a6a55c594e7f9f318b26ce398
rahul_thakur <rahulthakur.developer@gmail.com>
2024-06-06 09:44:57 +0530

    cofig service for login

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,3 +1,4 @@
+import JWT from 'jsonwebtoken'
 import { primsaClient } from "../lib/db"
 import {createHmac, randomBytes} from 'node:crypto'
 
@@ -8,19 +9,44 @@ export interface CreateUserPayload{
     password: string
 }
 
+export interface GetUserTokenPayload{
+    email: string
+    password: string
+}
+
 export class UserService{
     public static createUser (payload : CreateUserPayload){
         const {firstName, lastName, email, password} = payload
         const salt = randomBytes(32).toString('hex');
-        const hashedPassword = createHmac('sha256', salt).update(password).digest('hex');
         return primsaClient.user.create({
             data: {
                 firstName,
                 lastName,
                 email,
                 salt,
-                password: hashedPassword
+                password: UserService.generateHash(salt, password)
             }
         })
+    }
+
+    private static getUserByEmail(email: string){
+        return primsaClient.user.findUnique({where: {email}})
+    }
+
+    private static generateHash(salt: string , password: string){
+        return createHmac('sha256', salt).update(password).digest('hex') 
+    }
+
+    public static async getUserToken(payload: GetUserTokenPayload){
+        const {email, password} = payload;
+        const user = await UserService.getUserByEmail(email);
+        if(!user) throw new Error('Invalid email or password');
+
+        const userSalt = user.salt;
+        const userHashedPassword = UserService.generateHash(userSalt, password);
+        if(userHashedPassword !== user.password) throw new Error('Invalid email or password');
+
+        const token = JWT.sign({id: user.id,email: user.email}, process.env.JWT_SECRET || 'secret', {expiresIn: '1d'})
+        return token;
     }
 }
