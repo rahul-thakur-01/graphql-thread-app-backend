a93ceaf5602670b8e2d70328c5627aedae84221c
rahul_thakur <rahulthakur.developer@gmail.com>
2024-06-19 10:14:09 +0530

    add notes folder

--- src/lib/db.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.primsaClient = void 0;
-var client_1 = require("@prisma/client");
-exports.primsaClient = new client_1.PrismaClient();
